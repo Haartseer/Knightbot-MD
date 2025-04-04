918d55159562fdb7a419ac8ea5c97be588b0c80b
Haartseer <adeen6254@gmail.com>
2025-04-04 16:41:19 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
+  botOwner: 'Knight bot', // Your name
   ownerNumber: '27635326981', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
